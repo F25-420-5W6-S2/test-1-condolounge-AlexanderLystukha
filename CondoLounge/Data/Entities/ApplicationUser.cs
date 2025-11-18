7cf9c55eeec62864cc8371acb7db1ddc3b091855
Alexander Lystukha <158299750+AlexanderLystukha@users.noreply.github.com>
2025-11-18 11:46:12 -0500

    Added Condo, Building and User entities

--- CondoLounge/Data/Entities/ApplicationUser.cs
+++ CondoLounge/Data/Entities/ApplicationUser.cs
@@ -4,5 +4,7 @@ namespace CondoLounge.Data.Entities
 {
     public class ApplicationUser : IdentityUser<int>
     {
+        public ICollection<Condo> Condos { get; set; }
+        public ICollection<Building> Buildings { get; set; }
     }
 }
---
+++ CondoLounge/Data/Entities/Condo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CondoLounge.Data.Entities
+{
+    public class Condo
+    {
+        [Key]
+        public int Condo_Number { get; set; }
+        public ICollection<ApplicationUser> Users { get; set; }
+        public string Address { get; set; }
+    }
+}
